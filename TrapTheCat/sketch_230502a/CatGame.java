2ba34405b4b8841ee74c3ba3f13088e51f3a66d6
Taylor <ataylor@friendsbalt.org>
2023-05-02 09:56:46 -0400

    Find issue in starting with row 1 instead of 0

---
+++ TrapTheCat/sketch_230502a/CatGame.java
@@ -0,0 +1,148 @@
+import edu.princeton.cs.algs4.Edge;
+import edu.princeton.cs.algs4.Stack;
+
+public class CatGame {
+    public CatGraph graph;
+    public boolean[] marked;
+    public int catPos;
+    public int n;
+    public ShortestPathTree sp;
+    public boolean trapped;
+    
+    public CatGame(int n) {
+        trapped = false;
+        this.n = n;
+        graph = new CatGraph(n*n+1);
+        marked = new boolean[n*n];
+        catPos = index(n/2, n/2);
+        
+        //inner hexagons
+        for (int row = 0; row < n-1; row++) {
+            for (int col = 0; col < n-1; col++) {
+                int v = index(row, col);
+                
+                //add top left and right
+                if (row != 0) { //not on top row
+                    if (col != 0) { //if not on left side
+                        System.out.println(v + " -> " + (v-n));
+                        graph.addEdge(new CatEdge(v, v-n)); //top left
+                    }
+                    if (col != n) { //if not on right side
+                        System.out.println(v + " -> " + (v-n+1));
+                        graph.addEdge(new CatEdge(v, v-n+1)); //top right
+                    }
+                }
+                
+                //add bottom left and right
+                if (row != n) { //not on bottom row
+                    if (col != 0) { //if not on left side
+                    
+                        System.out.println(v + " -> " + (v+n-1));
+                        graph.addEdge(new CatEdge(v, v+n-1)); //bottom left
+                    }
+                    if (col != n) { //if not on right side
+                    
+                        System.out.println(v + " -> " + (v+n));
+                        graph.addEdge(new CatEdge(v, v+n)); //bottom right
+                    }
+                }
+                
+                //add left
+                if (col != 0) {
+                  
+                    System.out.println(v + " -> " + (v-1));
+                    graph.addEdge(new CatEdge(v, v-1)); //left
+                }
+                
+                //add right
+                if (col != n) {
+                    System.out.println(v + " -> " + (v+1));
+                    graph.addEdge(new CatEdge(v, v+1)); //right
+                }
+                
+                //if on border
+                if (col == 0 || col == n) {
+                    graph.addEdge(new CatEdge(v, n*n)); //freedom hexagon
+                }
+                if (row == 0 || row == n) {
+                  
+                        
+                    graph.addEdge(new CatEdge(v, n*n)); //freedom hexagon
+                }
+            }
+        }
+        
+        
+        //make separate for loop for outside tiles (one for each side?)
+        //make them connect to freedom hexagon
+        
+        for (int i = 0; i < marked.length; i ++) {
+            marked[i] = false;
+        }
+    }
+    
+    public void markTile(int row, int col) {
+        int v = index(row, col);
+        System.out.println(row);
+        System.out.println(col);
+        System.out.println(v);
+        
+        //update marked
+        marked[v] = true;
+        
+        //make adjacent edge weights infinity
+        for(Edge e : graph.adj(v)) {
+            CatEdge i = (CatEdge) e;
+            i.changeWeight();
+        }
+        
+        //dijkstra to move cat
+        sp = new ShortestPathTree(graph, catPos);
+        Stack<Edge> stack = (Stack<Edge>) sp.pathTo(n*n);
+        catPos = stack.pop().other(catPos);
+    }
+    
+    public void shadeTile(int row, int col) {
+        
+    }
+    
+    public boolean marked(int row, int col) {
+        int v = index(row, col);
+        return marked[v];
+    }
+    
+    public int[] getCatTile() {
+        int[] catTile = {getRow(catPos), getCol(catPos)};
+        return catTile;
+    }
+    
+    boolean catHasEscaped() {
+        return catPos == n*n;
+    }
+    
+    boolean catIsTrapped() {
+        trapped = true;
+        
+        for (Edge e : graph.adj(catPos)) {
+            CatEdge i = (CatEdge) e;
+            if (i.weight < Double.POSITIVE_INFINITY) {
+                trapped = false;
+            }
+        }
+        
+        return trapped;
+    }
+    
+    public int index(int row, int col) {
+        return row*n + col;
+    }
+    
+    public int getCol(int v) {
+        return v % n; //remainder
+    }
+    
+    public int getRow(int v) {
+        return v / n; //division
+    }
+    
+}
